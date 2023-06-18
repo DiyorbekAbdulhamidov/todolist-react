861ff20f424c5fd26d8c09d5d9e6df84353468b0
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-06-18 22:34:59 +0500

    codes

--- src/components/main/list.tsx
+++ src/components/main/list.tsx
@@ -3,23 +3,24 @@ import "./main.scss";
 
 interface ListProps {
   inputValue: string;
-  editBtn : string,
-  deleteBtn : string,
-  checkBtn : string
+  editBtn: string;
+  deleteBtn: string;
+  checkBtn: string;
+  onDelete: () => void;
+  onCheck: () => void;
 }
 
 export default class List extends React.Component<ListProps> {
   render() {
     const { inputValue, editBtn, deleteBtn, checkBtn } = this.props;
 
-
     return (
       <div className="list">
         <p>{inputValue}</p>
         <div className="icons">
           <span className="material-symbols-outlined editIcon">{editBtn}</span>
-          <span className="material-symbols-outlined deleteIcon">{deleteBtn}</span>
-          <span className="material-symbols-outlined checkIcon">{checkBtn}</span>
+          <span onClick={this.props.onDelete} className="material-symbols-outlined deleteIcon">{deleteBtn}</span>
+          <span onClick={this.props.onCheck} className="material-symbols-outlined checkIcon">{checkBtn}</span>
         </div>
       </div>
     );
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,8 +2,21 @@ import React from "react";
 import List from "./list";
 import "./main.scss";
 
-class Main extends React.Component {
-  state = {
+interface ListItem {
+  key: string;
+  inputValue: string;
+  editBtn: string;
+  deleteBtn: string;
+  checkBtn: string;
+}
+
+interface MainState {
+  listInputValue: string;
+  list: ListItem[];
+}
+
+class Main extends React.Component<{}, MainState> {
+  state: MainState = {
     listInputValue: "",
     list: []
   };
@@ -12,32 +25,45 @@ class Main extends React.Component {
     this.addListElement();
   };
 
-  handleInputChange = (e:any) => {
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ listInputValue: e.target.value });
   };
 
-  handleKey = (e: any) => {
+  handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       this.addListElement();
     }
   };
 
+  handleDelete = (item: string) => {
+    const updatedList = this.state.list.filter((listItem) => listItem.key !== item);
+    this.setState({ list: updatedList });
+  };
+
+  handleCheck = (item: string) => {
+    // const checkedList = this.state.list.map((listItem) => {
+    //   if (listItem.key === item) {
+    //     return { ...listItem, checked: true };
+    //   }
+    //   return listItem;
+    // });
+    // this.setState({ list: checkedList });
+  };
+
   addListElement = () => {
     const { listInputValue } = this.state;
     if (listInputValue) {
-      const listElement = (
-        <List
-          key={listInputValue}
-          inputValue={listInputValue}
-          editBtn="edit"
-          deleteBtn="delete"
-          checkBtn="check_circle"
-        />
-
-      );
+      const listElement: ListItem = {
+        key: listInputValue,
+        inputValue: listInputValue,
+        editBtn: "edit",
+        deleteBtn: "delete",
+        checkBtn: "check_circle"
+      };
       const newList = [...this.state.list, listElement];
       this.setState({ list: newList, listInputValue: "" });
-    } else {
+    }
+    else {
       alert("Iltimos Inputga Qiymat kiriting!🙌");
     }
   };
@@ -50,16 +76,21 @@ class Main extends React.Component {
         <h1>Welcome To Do List!</h1>
         <div className="toDoList">
           <div className="inpBtn">
-            <input
-              type="text"
-              placeholder="Write To Do"
-              value={listInputValue}
-              onChange={this.handleInputChange}
-              onKeyPress={this.handleKey}
+            <input type="text" placeholder="Write To Do" value={listInputValue} onChange={this.handleInputChange} onKeyPress={this.handleKey}
             />
             <button onClick={this.handleClick}>Add To Do</button>
           </div>
-          {list}
+          {list.map((item) => (
+            <List
+              key={item.key}
+              inputValue={item.inputValue}
+              editBtn={item.editBtn}
+              deleteBtn={item.deleteBtn}
+              checkBtn={item.checkBtn}
+              onDelete={() => this.handleDelete(item.key)}
+              onCheck={() => this.handleCheck(item.key)}
+            />
+          ))}
         </div>
       </div>
     );
